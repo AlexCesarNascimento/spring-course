ec6e21e3b75913ac96587f28ae902be83a10d64c
Alex Cesar Nascimento <66449173+AlexCesarNascimento@users.noreply.github.com>
2020-11-22 11:27:51 -0300

    Create user login dto class

--- src/main/java/com/springcourse/repository/RequestStageRepository.java
+++ src/main/java/com/springcourse/repository/RequestStageRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import com.springcourse.domain.RequestStage;
 
 @Repository
